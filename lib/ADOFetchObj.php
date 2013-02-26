9d3864c7813eadcf76642ce642df85f5bb385668
Sam Williams <samuel.williams8@gmail.com>
2013-02-26 13:49:45 +1100

    Using composer.

---
+++ lib/ADOFetchObj.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Internal placeholder for record objects. Used by ADORecordSet->FetchObj().
+ */
+class ADOFetchObj {}
---
+++ lib/ADOFieldObject.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Helper class for FetchFields -- holds info on a column
+ */
+class ADOFieldObject
+{
+    public $name = '';
+    public $max_length = 0;
+    public $type = "";
+}
